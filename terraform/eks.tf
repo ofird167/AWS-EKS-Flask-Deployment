c7aad46b63f5c3dab48cbec5cf03b03a600ff47e
DevOps Engineer <engineer@example.com>
2026-05-28 22:26:07 +0300

    chore: enable force_delete on ECR repository to facilitate clean destroy

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -4,6 +4,7 @@
 resource "aws_ecr_repository" "interview1_ecr" {
   name                 = "interview1-app"
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = true
