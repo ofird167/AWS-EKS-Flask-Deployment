44534b092a5a4475ea71a281f2f08413e2fd8b0f
ofird167 <ofird167@gmail.com>
2026-06-22 20:07:29 +0300

    chore: standardize environment configs and secrets layout

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,12 +9,7 @@ terraform {
   }
 
   # שמירת ה-State בצורה מאובטחת ב-S3 בתוך us-east-1
-  backend "s3" {
-    bucket         = "s3AWS-EKS-Flask-Deployment-state-bucket-ofir" # השם שייצרת בשלב 1
-    key            = "s3AWS-EKS-Flask-Deployment/terraform.tfstate"
-    region         = "us-east-1"
-    encrypt        = true
-  }
+  backend "s3" {}
 }
 
 provider "aws" {
